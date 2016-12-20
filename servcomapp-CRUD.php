a710dc00a200bc1741c8201e92205acbc9d672ed
Cristhian Caicedo <cristh2592@gmail.com>
2016-12-20 15:36:45 -0430

    WordPress Plugin that manages the CRUD of an information system

---
+++ servcomapp-CRUD.php
@@ -0,0 +1,86 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/**
+ * Este es el archivo de arranque del Plugin
+ *
+ * Este archivo es leido por WordPress para generar la informacion del plugin en
+ * la area de administracion del plugin. Este archivo tambien incluye todas las
+ * dependencias usadas por el plugin, registra las funciones de activacion y
+ * desactivacion, y define una funcion que inicia el plugin.
+ *
+ * @link              
+ * @since             1.0.0
+ * @package           servcomapp CRUD
+ *
+ * @wordpress-plugin
+ * Plugin Name:       servcomapp CRUD
+ * Plugin URI:        #
+ * Description:       Plugin que maneja la data del sistema de informacion
+ * Version:           1.0.0
+ * Author:            Cristhian Caicedo
+ * Author URI:        #
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:
+ * Domain Path:       /languages
+ */
+
+// Si este archivo es llamado directamente, abortar por seguridad.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Cabe destacar que como el plugin esta siendo colocado dentro de la carpeta
+ * "mu-plugins", el script activador, desactivador y desinstalador no seran
+ * leidos por WordPress debido a que estos plugins son colocados para que no
+ * sean alterados por ningun usuario, sino que son permanentes y sumamente
+ * importantes para el sistema de informacion. Debe estar siempre activo y no
+ * puede ser desinstalado sino manualmente. No obstante, se deja el codigo
+ * necesario por si se desea trabajar de la otra manera, es decir, colocando
+ * el plugin en la carpeta "plugins" para ser manejado por los usuarios del
+ * escritorio de WordPress.
+ *
+ * El codigo que corre durante la activacion del Plugin.
+ * Esta accion es documentada en includes/class-activador-nombre-plugin.php
+ */
+function activar_nombre_plugin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * El codigo que corre durante la desactivacion del Plugin.
+ * Esta accion es documentada en includes/class-plugin-name-deactivator.php
+ */
+function desactivar_nombre_plugin() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activar_nombre_plugin' );
+register_deactivation_hook( __FILE__, 'desactivar_nombre_plugin' );
+
+/**
+ * La clase central del plugin que es usada para definir internacionalizacion,
+ * hooks especificos del administrador,y hooks de la pagina web orientados al
+ * publico.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+
+/**
+ * Comienza la ejecucon del plugin.
+ *
+ * Dado que todo dentro del plugin es registrado mediante hooks,
+ * entonces arrancar el plugin desde este punto en el archivo no afecta el ciclo
+ * de vida de la pagina.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
+}
+run_plugin_name();
---
+++ uninstall.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Se ejecuta cuando el plugin es desinstalado.
+ *
+ * Cuando se vaya a añadir contenido a este archivo, considerar el siguiente
+ * flujo de control:
+ *
+ * - Este metodo deberia ser estatico
+ * - Chequear si el contenido de $_REQUEST hace referencia al nombre del plugin
+ * - Ejecute un chequeo de referencia de administrador para estar seguro de que
+ *   pasa por el proceso de autentificacion
+ * - Verifique que la salida de $_GET tenga sentido
+ * - Repita con otros roles de usuarios. Lo mejor es que se haga directamente
+ *   usando los parametros de cadenas de caracteres de links/query.
+ * - Repita cosas para el multisitio. Una vez para un unico sitio web en la red,
+ *   una vez a nivel de sitio web.
+ *
+ * Esta version podria ser actualizada en la version futura del Boilerplate.Sin
+ * embargo, este es el esqueleto general y guia para describir como el archivo
+ * deberia trabajar.
+ *
+ * Para mas informacion acerca de esto, vea la siguiente discusion:
+ * https://github.com/tommcfarlin/WordPress-Plugin-Boilerplate/pull/123#issuecomment-28541913
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    servcomapp CRUD
+ */
+
+// Si la desinstalacion no fue llamada desde WordPress, entonces se sale.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
